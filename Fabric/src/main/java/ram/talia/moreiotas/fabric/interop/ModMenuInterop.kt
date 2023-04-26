11f0a62164df622d086012a0d856d7af272964e9
Talia-12 <paradox.m.burgess@gmail.com>
2023-04-26 18:22:40 +1000

    fixed REI bug on servers.

--- Fabric/src/main/java/ram/talia/moreiotas/fabric/interop/ModMenuInterop.kt
+++ Fabric/src/main/java/ram/talia/moreiotas/fabric/interop/ModMenuInterop.kt
@@ -1,16 +1,16 @@
 package ram.talia.moreiotas.fabric.interop
 
-import at.petrak.hexcasting.fabric.FabricHexConfig
 import com.terraformersmc.modmenu.api.ConfigScreenFactory
 import com.terraformersmc.modmenu.api.ModMenuApi
 import me.shedaniel.autoconfig.AutoConfig
 import net.fabricmc.api.EnvType
 import net.fabricmc.api.Environment
 import net.minecraft.client.gui.screens.Screen
+import ram.talia.moreiotas.fabric.FabricMoreIotasConfig
 
 @Environment(EnvType.CLIENT)
 class ModMenuInterop : ModMenuApi {
-    override fun getModConfigScreenFactory(): ConfigScreenFactory<*>? {
-        return ConfigScreenFactory { parent: Screen? -> AutoConfig.getConfigScreen(FabricHexConfig::class.java, parent).get() }
+    override fun getModConfigScreenFactory(): ConfigScreenFactory<*> {
+        return ConfigScreenFactory { parent: Screen? -> AutoConfig.getConfigScreen(FabricMoreIotasConfig::class.java, parent).get() }
     }
 }
--- Fabric/src/main/java/ram/talia/moreiotas/fabric/interop/rei/MoreIotasREIPlugin.java
+++ Fabric/src/main/java/ram/talia/moreiotas/fabric/interop/rei/MoreIotasREIPlugin.java
@@ -2,11 +2,12 @@ package ram.talia.moreiotas.fabric.interop.rei;
 
 import me.shedaniel.rei.api.client.plugins.REIClientPlugin;
 import me.shedaniel.rei.api.client.registry.category.CategoryRegistry;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 
+@Environment(EnvType.CLIENT)
 public class MoreIotasREIPlugin implements REIClientPlugin {
 	
 	@Override
-	public void registerCategories (CategoryRegistry registry) {
-		return;
-	}
+	public void registerCategories (CategoryRegistry registry) { }
 }
