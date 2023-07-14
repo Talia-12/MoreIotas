008943f9825ed185e91a7b12d344671762f6e9da
Talia-12 <paradox.m.burgess@gmail.com>
2023-07-14 16:22:05 +1000

    Fix Patternmaster's not recognising Special patterns. (#10)

--- Common/src/main/java/ram/talia/moreiotas/common/casting/actions/strings/OpActionString.kt
+++ Common/src/main/java/ram/talia/moreiotas/common/casting/actions/strings/OpActionString.kt
@@ -4,8 +4,10 @@ import at.petrak.hexcasting.api.PatternRegistry
 import at.petrak.hexcasting.api.spell.ConstMediaAction
 import at.petrak.hexcasting.api.spell.asActionResult
 import at.petrak.hexcasting.api.spell.casting.CastingContext
+import at.petrak.hexcasting.api.spell.casting.SpecialPatterns.*
 import at.petrak.hexcasting.api.spell.getPattern
 import at.petrak.hexcasting.api.spell.iota.Iota
+import net.minecraft.network.chat.Component
 import ram.talia.moreiotas.api.asActionResult
 
 /**
@@ -16,6 +18,13 @@ object OpActionString : ConstMediaAction {
 
     override fun execute(args: List<Iota>, ctx: CastingContext): List<Iota> {
         val pattern = args.getPattern(0, argc)
+
+        if (pattern.sigsEqual(INTROSPECTION))
+            return Component.translatable("hexcasting.spell.hexcasting:open_paren").string.asActionResult
+        if (pattern.sigsEqual(RETROSPECTION))
+            return Component.translatable("hexcasting.spell.hexcasting:close_paren").string.asActionResult
+        if (pattern.sigsEqual(CONSIDERATION))
+            return Component.translatable("hexcasting.spell.hexcasting:escape").string.asActionResult
 
         val action = PatternRegistry.lookupPatternByShape(pattern) ?: return null.asActionResult
 
